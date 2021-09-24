724beb9c8484e6fe0ed20620657069fe543f35fc
Hannibal <hewiefsociety@gmail.com>
2021-09-24 14:49:49 +0800

    Update convert2aria2.php

--- convert2aria2.php
+++ convert2aria2.php
@@ -8,15 +8,18 @@ function read_file($path) {
         fclose($handle);
     }
 }
-$glob = read_file('origin_url.txt');
+$glob = read_file('xxxx.myshopify.com.txt');
 while ($glob->valid()) {
     $line = $glob->current();
 
     if(strstr($line,"https://")){
        $line=substr($line,0,strrpos($line,'?'));
-       $str=$line.PHP_EOL." out=".str_replace("https://","xxx.myshopify.com/",$line).PHP_EOL;
-       echo $str;
-       file_put_contents("download_list.txt",$str,FILE_APPEND);
+       if($line){
+         $str=$line.PHP_EOL." out=".str_replace("https://","xxxx.myshopify.com/",$line).PHP_EOL;
+         echo $str;
+         file_put_contents("download_list.txt",$str,FILE_APPEND);
+       }
+
     }
 
     $glob->next();
